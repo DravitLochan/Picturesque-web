20c6d24898bf45771ec136ba671a5c7a76ca2232
DravitLochan <dlochangupta@gmail.com>
2017-01-31 00:49:19 +0530

    size and extension check of a file

--- upload.php
+++ upload.php
@@ -2,7 +2,8 @@
 $name = $_FILES['file']['name'];
 $type = $_FILES['file']['type'];
 $size=$_FILES['file']['size'];
-
+$extension = strtolower(substr($name, strpos($name,'.')+1));
+$max_size=2097152;
 $tmp_name = $_FILES['file']['tmp_name'];
 $error = $_FILES['file']['error'];
 
@@ -11,11 +12,18 @@ if(isset($name))					//check if form is submited
 	if(!empty($name))				//check if the file uploaded is not empty
 	{
 		$location='uploads/';		//define a location for the files.
-		if(move_uploaded_file($tmp_name, $location.$name))
+		if(($extension=='jpg'||$extension=='jpeg')&&($type=='image/jpeg'||$type=='image/jpg')&&$size<$max_size)
 		{
-			echo 'uploaded successfully!<br>';
-			echo 'file size = '.$size.' Bytes<br>';
-			echo 'file type -> '.$type.'<br>';
+			if(move_uploaded_file($tmp_name, $location.$name))
+			{
+				echo 'uploaded successfully!<br>';
+				echo 'file size = '.$size.' Bytes<br>';
+				echo 'file type -> '.$type.'<br>';
+			}
+		}
+		else
+		{
+			echo 'file must be less than 2mb in size and of jpg or jpeg type!';
 		}
 	}
 	else
